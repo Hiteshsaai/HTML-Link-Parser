29101c3ce44f13b6dbed9c698bce2b1befbd7436
System Administrator <mppk2009@gmail.com>
2021-01-25 21:02:53 -0700

    Bug cleared in the package link

--- link/link.go
+++ link/link.go
@@ -1,5 +1,7 @@
 package link
 
+import "io"
+
 // This Link represents the links in the HTML tag
 type Link struct {
 	href string
@@ -8,6 +10,7 @@ type Link struct {
 
 // Parse will take an HTML document and it will
 // return slices
-func Parser(r io.reader) ([]Link, error) {
+
+func Parser(r io.Reader) ([]Link, error) {
 	return nil, nil
 }
